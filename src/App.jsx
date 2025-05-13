aa9d1f13cfaf2d4cdda40bb52102099c6c3b56c8
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-13 16:41:52 +0200

    [FIX] img and card size

--- src/App.jsx
+++ src/App.jsx
@@ -25,17 +25,17 @@ function App() {
         </div>
         <div className="row g-3">
           {actress.map((female) => (
-            <div key={`female-${female.id}`} className='col-12 col-md-6 col-lg-4'>
-              <div className='card rounded-0 flex-row bg-dange'>
+            <div key={`female-${female.id}`} className='col-12 col-md-6 col-lg-3'>
+              <div className='card rounded-0'>
                 <div className="actress-img img-thumbnail">
                   <img src={female.image} alt="" />
-                </div>
-                <div className='actress-infos text-bg-danger p-2'>
-                  <h3 className='actress-name'>{female.name}</h3>
-                  <p className='actress-birth'>{female.birth_year}</p>
-                  <p className='actress-nation'>{female.nationality}</p>
-                  <p className="actress-bio">{female.biography}</p>
-                  <p className="actress-awards">{female.awards}</p>
+                  <div className='actress-infos text-bg-danger p-2'>
+                    <h3 className='actress-name'>{female.name}</h3>
+                    <p className='actress-birth'>{female.birth_year}</p>
+                    <p className='actress-nation'>{female.nationality}</p>
+                    <p className="actress-bio">{female.biography}</p>
+                    <p className="actress-awards">{female.awards}</p>
+                  </div>
                 </div>
               </div>
             </div>
