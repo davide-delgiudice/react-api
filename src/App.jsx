b949ad88d6eb95e21d3b4cd21e83bacc7fdae405
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-13 16:36:08 +0200

    [UPD] add h1 margin and text card bg color

--- src/App.jsx
+++ src/App.jsx
@@ -26,11 +26,11 @@ function App() {
         <div className="row g-3">
           {actress.map((female) => (
             <div key={`female-${female.id}`} className='col-12 col-md-6 col-lg-4'>
-              <div className='card rounded-0'>
-                <div className="actress-img">
+              <div className='card rounded-0 flex-row bg-dange'>
+                <div className="actress-img img-thumbnail">
                   <img src={female.image} alt="" />
                 </div>
-                <div className='actress-infos'>
+                <div className='actress-infos text-bg-danger p-2'>
                   <h3 className='actress-name'>{female.name}</h3>
                   <p className='actress-birth'>{female.birth_year}</p>
                   <p className='actress-nation'>{female.nationality}</p>
