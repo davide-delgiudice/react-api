1b4223d565f03adf43b220546057f6830428b97c
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-13 15:17:08 +0200

    [UPD] made axios call and create state and effect for fetchActress

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+
 function App() {
+
+  const[actress, setActress] = useState([]);
+
+  const fetchActress = () => {
+    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((response) => setActress(response.data.results))
+  };
+
+  useEffect(() =>{
+    fetchActress();
+  }, []);
 
   return (
     <>
